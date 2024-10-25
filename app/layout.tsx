8f3de6692229100efe64bf29b69f5b5fcedda778
shoaib031 <kmem98@gmail.com>
2024-10-25 20:27:20 +0500

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
