e133d6a1690bfc2f88ffa23c2b7f66d3fecd69a6
shoaib031 <kmem98@gmail.com>
2024-10-25 20:42:01 +0500

    changess

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,17 +1,7 @@
 import React from "react";
 
-interface MeetingProps {
-  params: {
-    id: string;
-  };
-}
-
-const Meeting: React.FC<MeetingProps> = ({ params }) => {
+const Meeting = ({ params }: { params: { id: string } }) => {
   return <div>Meeting Room: # {params.id}</div>;
 };
 
-// Export the component as the default export
 export default Meeting;
-
-// Use the following function to specify dynamic route segments (if needed):
-export const dynamic = "force-dynamic"; // This will ensure the component is rendered dynamically
