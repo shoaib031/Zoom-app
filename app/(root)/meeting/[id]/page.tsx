d2d6d4ae272b3b0ad30d59bc8a7f871eada82332
shoaib031 <kmem98@gmail.com>
2024-10-25 20:31:57 +0500

    change

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-const Meeting = ({ params }: { params: { id: string } }) => {
+interface MeetingProps {
+  params: {
+    id: string;
+  };
+}
+
+const Meeting: React.FC<MeetingProps> = ({ params }) => {
   return <div>Meeting Room: # {params.id}</div>;
 };
+
+// Add getServerSideProps here, outside the Meeting component
+export async function getServerSideProps(context: { params: { id: string } }) {
+  return {
+    props: {
+      params: context.params,
+    },
+  };
+}
 
 export default Meeting;
