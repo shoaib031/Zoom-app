28800d16cdbc83163ee92e1998d2be1310027fd9
shoaib031 <kmem98@gmail.com>
2024-10-28 12:52:20 +0500

    error

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,5 +1,18 @@
-// // app/meeting/[id]/page.tsx
-// import { NextPage } from "next";
+// // // app/meeting/[id]/page.tsx
+// // import { NextPage } from "next";
+
+// // type MeetingProps = {
+// //   params: {
+// //     id: string;
+// //   };
+// // };
+
+// // const Meeting: NextPage<MeetingProps> = ({ params }) => {
+// //   return <div>Meeting Room: #{params.id}</div>;
+// // };
+
+// // export default Meeting;
+
 
 // type MeetingProps = {
 //   params: {
@@ -7,21 +20,23 @@
 //   };
 // };
 
-// const Meeting: NextPage<MeetingProps> = ({ params }) => {
+// const Meeting = ({ params }: MeetingProps) => {
 //   return <div>Meeting Room: #{params.id}</div>;
 // };
 
 // export default Meeting;
 
 
+import { GetServerSidePropsContext } from 'next';
 type MeetingProps = {
   params: {
     id: string;
   };
 };
-
 const Meeting = ({ params }: MeetingProps) => {
   return <div>Meeting Room: #{params.id}</div>;
 };
-
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  return { props: { params: context.params } };
+}
 export default Meeting;
